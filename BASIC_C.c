452831c0658fc21b22fcad1ffc1acb8fc0d386f4
NISHU <amritrajdutta99725@gmail.com>
2026-09-02 14:10:39 +0530

    Code is Updated with New Functions

--- BASIC_C.c
+++ BASIC_C.c
@@ -1,47 +1,94 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include<string.h>
 
 int main()
 {
-    // In this section of topics we dicuss some stuff about Arithmetic operators , Format Specifiers , precions flags etc
-    // So we have the basic information about these topics ,but we need to sharpen much more . 
-    // Format specifers = It is used to represent the types of data to be gets printed => %d,%c,%s,%f,%x,%lf 
-    // %d => For Integer , %c => For Character , %s => For String , %f => For Float , %x => For Hexadecimal , %lf +> Double 
-    // Examples given Below :
-    int age ; char ch ; float Marks ; double IP ; char name[30];
-    //{
+    char ch ;
+    printf("Enter The Choice : 1. QE , 2. SQ Root , 3. AOT By Herons Formula , 4. distance relationships \n");
+    if (scanf("%c", &ch) != 1) {
+        return 0;
+    }
 
-    printf("Enter The Value Age in the Form of integer\n");
-    scanf("%d",&age);
-    printf("Age = %d\n",age);
-
-
-    printf("Enter One Character\n"); 
-    scanf(" %c",&ch); 
-    printf("Char = %c\n",ch); // It takes the char input 
-
-
-
-    printf("Enter The Marks(cgpa) in Decimal Form\n");
-    scanf(" %f",&Marks);
-    printf("Marks in CGPA = %.3f\n",Marks);
-
-
-    printf("Enter The Value Of Internet Protocol(IP)\n");
-    scanf(" %lf",&IP);
-    printf("Value of Internet Protocol (IP)= %.7f\n",IP);
-
-getchar();
-
-    printf("Enter Your Name\n"); 
-    fgets(name, sizeof(name), stdin);
-    printf("Your Name Is => %s",name);
-
-
-
-    //}
+    switch (ch) {
+        case 1: {
+            double a, b, c, discriminant, root1, root2;
+            printf("Enter The Value of A , B , C to find Roots\n");
+            if (scanf("%lf%lf%lf", &a, &b, &c) != 3) {
+                return 0;
+            }
+            if (a == 0) {
+                printf("Not a quadratic equation.\n");
+                break;
+            }
+            discriminant = b * b - 4 * a * c;
+            if (discriminant > 0) {
+                printf("REAL AND DISTINCT ROOTS EXIST\n");
+                root1 = (-b + sqrt(discriminant)) / (2 * a);
+                root2 = (-b - sqrt(discriminant)) / (2 * a);
+                printf("ROOT_1 = %.3f\n", root1);
+                printf("ROOT_2 = %.3f\n", root2);
+            } else if (discriminant == 0) {
+                root1 = -b / (2 * a);
+                printf("REAL AND EQUAL ROOTS EXIST\n");
+                printf("ROOT = %.3f\n", root1);
+            } else {
+                double real = -b / (2 * a);
+                double imag = sqrt(-discriminant) / (2 * a);
+                printf("COMPLEX ROOTS EXIST\n");
+                printf("ROOT_1 = %.3f + %.3fi\n", real, imag);
+                printf("ROOT_2 = %.3f - %.3fi\n", real, imag);
+            }
+            break;
+        }
+        case 2: {
+            double n;
+            printf("Enter The Number to find square root\n");
+            if (scanf("%lf", &n) != 1) {
+                return 0;
+            }
+            if (n < 0) {
+                printf("Negative number, square root is complex.\n");
+            } else {
+                printf("Square root = %.3f\n", sqrt(n));
+            }
+            break;
+        }
+        case 3: {
+            double a, b, c, s, area;
+            printf("Enter the three sides of the triangle\n");
+            if (scanf("%lf%lf%lf", &a, &b, &c) != 3) {
+                return 0;
+            }
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+                printf("Invalid triangle sides\n");
+            } else {
+                s = (a + b + c) / 2;
+                area = sqrt(s * (s - a) * (s - b) * (s - c));
+                printf("Area = %.3f\n", area);
+            }
+            break;
+        }
+        case 4: {
+            double x1, y1, x2, y2, distance, midx, midy;
+            printf("Enter coordinates of first point (x1 y1)\n");
+            if (scanf("%lf%lf", &x1, &y1) != 2) {
+                return 0;
+            }
+            printf("Enter coordinates of second point (x2 y2)\n");
+            if (scanf("%lf%lf", &x2, &y2) != 2) {
+                return 0;
+            }
+            distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            midx = (x1 + x2) / 2.0;
+            midy = (y1 + y2) / 2.0;
+            printf("Distance = %.3f\n", distance);
+            printf("Midpoint = (%.3f, %.3f)\n", midx, midy);
+            break;
+        }
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
 
     return 0;
 }
